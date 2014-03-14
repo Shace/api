a0a082e5f2cf686e4f76d96fcd3f73070a5385f2
Melvin Laplanche <melvin.laplanche+dev@gmail.com>
2014-03-14 14:14:54 -0700

    Use the DATABASE_URL variable

--- conf/env.example.sh
+++ conf/env.example.sh
@@ -6,6 +6,4 @@ export MEDIA_ROOT_URL='8mtC^AGeQs`=ImcCTmGp^oKhjDr`IHs:n77isJy;Poko?KEHskTixTXCm
 
 # The commented part are for a basic mysql installation
 export DB_DRIVER="org.postgresql.Driver" # "com.mysql.jdbc.Driver"
-export DB_URL="jdbc:postgresql://127.0.0.1:5432/shace" # "jdbc:mysql://127.0.0.1:3306/shace"
-export DB_USER="postgres" # "root"
-export DB_PSW="postgres" # ""
+export DATABASE_URL="postgresql://postgres:postgres@127.0.0.1:5432/shace" # "mysql://root@127.0.0.1:3306/shace"
