bb1a0a9d06945de693f50b2bfb6ea411341c71b0
Melvin Laplanche <melvin.laplanche+dev@gmail.com>
2014-03-16 12:56:41 -0700

    Fix env

--- conf/env.example.sh
+++ conf/env.example.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 export MEDIA_PATH=`pwd`/../public/media/
-export MEDIA_ROOT_URL="127.0.0.1:9000"
+export MEDIA_ROOT_URL="//127.0.0.1:9000/assets/medias/"
 export SECRET_KEY='8mtC^AGeQs`=ImcCTmGp^oKhjDr`IHs:n77isJy;Poko?KEHskTixTXCmkUltiEx'
 
 # The commented part are for a basic mysql installation
