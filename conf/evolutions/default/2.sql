d703297b09dd808862da988556f9fa9863b68cc8
Samuel Olivier <skanight@hotmail.fr>
2014-08-27 11:14:13 +0200

    Resolve Evolution bug for admin lastname

--- conf/evolutions/default/2.sql
+++ conf/evolutions/default/2.sql
@@ -10,8 +10,8 @@ INSERT INTO se_image_format
 				   	('cover', 849, 313, true, 1);
 
 INSERT INTO se_user
-       (id, email, password, first_name, is_admin, inscription_date, lang)
-       VALUES (0, 'admin@shace.io', '3ed4fd5119338b75e9fbcf7a1279f1f5b0025b64', 'Admin', true, Now(), 2);
+       (id, email, password, first_name, last_name, is_admin, inscription_date, lang)
+       VALUES (0, 'admin@shace.io', '3ed4fd5119338b75e9fbcf7a1279f1f5b0025b64', 'Admin', '', true, Now(), 2);
 
 # --- !Downs
 
