5792e140949f22fa4f505df55e3fc6c7b77fcf33
loick-michard <loick.michard@gmail.com>
2014-09-14 20:43:16 +0200

    Add missing sql

---
+++ conf/evolutions/default/8.sql
@@ -0,0 +1,9 @@
+
+# --- !Ups
+
+INSERT INTO se_image_format
+       (name, width, height, crop, type)
+       VALUES 		('square', 195, 195, true, 0),
+				   	('bigCover', 1170, 350, true, 1)
+
+# --- !Downs
